9735992c2712e17f381b982bffab1777dd630fa2
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-24 02:13:27 -0500

    feat:
    Ya se puede registrar

--- src/main/java/com/mycompany/proyectojava/ProyectoJava.java
+++ src/main/java/com/mycompany/proyectojava/ProyectoJava.java
@@ -1,4 +1,18 @@
 package com.mycompany.proyectojava;
 
+import com.mycompany.proyectojava.View.Mascota.MascotaView;
+import com.mycompany.proyectojava.controller.Mascota.MascotaControlller;
+import com.mycompany.proyectojava.repository.Mascota.IMascota;
+import com.mycompany.proyectojava.repository.Mascota.MascotaDAO;
+
 public class ProyectoJava {
+    public static void main(String[] args) {
+        IMascota dao = new MascotaDAO();
+
+        MascotaControlller controller = new MascotaControlller(dao);
+
+        MascotaView view = new MascotaView(controller);
+
+        view.mostrarMenu();
+    }
 }
