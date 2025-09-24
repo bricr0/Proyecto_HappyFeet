2b3f672129309d2574e6cc518680e3098d314621
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-24 16:57:21 -0500

    feat: add methods for managing mascotas in IMascota and MascotaDAO

--- src/main/java/com/mycompany/proyectojava/repository/Mascota/IMascota.java
+++ src/main/java/com/mycompany/proyectojava/repository/Mascota/IMascota.java
@@ -2,6 +2,41 @@ package com.mycompany.proyectojava.repository.Mascota;
 
 import com.mycompany.proyectojava.model.entities.Mascota.Mascota;
 
+import java.util.List;
+
 public interface IMascota {
+
+//    ---------------------------------------------------1. AGREGAR MASCOTA ------------------------------------------------
+
     void agregarMascota(Mascota mascota);
+
+//    ---------------------------------------------------2. LISTAR MASCOTAS ------------------------------------------------
+
+    List<Mascota> listarTodas();
+
+//    ---------------------------------------------------3. LISTAR MASCOTAS ACTIVAS ------------------------------------------------
+    List<Mascota> listarActivas();
+
+//    ---------------------------------------------------4. LISTAR MASCOTAS INACTIVAS ------------------------------------------------
+    List<Mascota> listarInactivas();
+
+//    ---------------------------------------------------5. BUSCAR MASCOTA ------------------------------------------------
+    Mascota listarPorMicrochip(String microchip);
+
+//    ---------------------------------------------------6. ACTUALIZAR MASCOTA ------------------------------------------------
+    void actualizarMascota(Mascota mascota);
+
+//    -----------------------------------------------------------7. ELIMINAR MASCOTA ------------------------------------------------
+    void eliminarMascota(String microchip);
+
+//    -----------------------------------------------------------8. CREAR ESPECIE ------------------------------------------------
+    void crearEspecie(String especie);
+
+//    -----------------------------------------------------------9. CREAR RAZA ------------------------------------------------
+    void crearRaza(String raza, String especie);
+
 }
+
+
+
+
--- src/main/java/com/mycompany/proyectojava/repository/Mascota/MascotaDAO.java
+++ src/main/java/com/mycompany/proyectojava/repository/Mascota/MascotaDAO.java
@@ -5,6 +5,7 @@ import com.mycompany.proyectojava.model.entities.Mascota.Mascota;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.List;
 
 public class MascotaDAO implements IMascota {
     private Connection conexion;
@@ -13,6 +14,8 @@ public class MascotaDAO implements IMascota {
         this.conexion = ConexionDBSingleton.getInstance().getConnection();
     }
 
+
+//    -------------------------------------------------------------------------------1. AGREGAR MASCOTA ---------------------------------------------------------------------------------
     @Override
     public void agregarMascota(Mascota mascota) {
         String sql = "INSERT INTO mascotas (dueno_id, nombre, raza_id, fecha_nacimiento, sexo, microchip, alergias, condiciones_preexistentes, peso_kg) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -31,5 +34,61 @@ public class MascotaDAO implements IMascota {
         } catch (Exception e) {
             throw new RuntimeException("Error al agregar una mascota: " + e);
         }
+    }
+
+//    -------------------------------------------------------------------------------2. LISTAR MASCOTAS ---------------------------------------------------------------------------------
+
+    @Override
+    public List<Mascota> listarTodas() {
+        return List.of();
+    }
+
+//    -------------------------------------------------------------------------------3. LISTAR MASCOTAS ACTIVAS ---------------------------------------------------------------------------------
+
+    @Override
+    public List<Mascota> listarActivas() {
+        return List.of();
+    }
+
+//    --------------------------------------------------------------------------------4. LISTAR MASCOTAS INACTIVAS ---------------------------------------------------------------------------------
+
+    @Override
+    public List<Mascota> listarInactivas() {
+        return List.of();
+    }
+
+//    --------------------------------------------------------------------------------5. LISTAR MASCOTAS MICROCHIP ---------------------------------------------------------------------------------
+
+    @Override
+    public Mascota listarPorMicrochip(String microchip) {
+        return null;
+    }
+
+//    --------------------------------------------------------------------------------6. ACTUALIZAR MASCOTA ---------------------------------------------------------------------------------
+
+    @Override
+    public void actualizarMascota(Mascota mascota) {
+
+    }
+
+//    --------------------------------------------------------------------------------7. ELIMINAR MASCOTA ---------------------------------------------------------------------------------
+
+    @Override
+    public void eliminarMascota(String microchip) {
+
+    }
+
+//    --------------------------------------------------------------------------------8. CREAR ESPECIE ---------------------------------------------------------------------------------
+
+    @Override
+    public void crearEspecie(String especie) {
+
+    }
+
+//    ---------------------------------------------------------------------------------9. CREAR RAZA ---------------------------------------------------------------------------------
+
+    @Override
+    public void crearRaza(String raza, String especie) {
+
     }
 }
